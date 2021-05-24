2d9442fd9edfeb25cf45dabedacfef258e79417a
paridhi-kasliwal <paridhik1994@gmail.com>
2021-05-24 13:30:55 +0530

    updated progress bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
+  {path: '', redirectTo: 'sites', pathMatch: 'full' },
   {path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)},
   {path: 'sites', loadChildren: () => import('./sites/sites.module').then(m => m.SitesModule)},
   {path: 'migrations', loadChildren: () => import('./migrations/migrations.module').then(m => m.MigrationsModule)},
@@ -9,7 +10,6 @@ const routes: Routes = [
   {path: 'collaborators', loadChildren: () => import('./collaborators/collaborators.module').then(m => m.CollaboratorsModule)},
   {path: 'support-tickets', loadChildren: () => import('./support-tickets/support-tickets.module').then(m => m.SupportTicketsModule)},
   {path: 'open-new-ticket', loadChildren: () => import('./open-new-tickets/open-new-tickets.module').then(m => m.OpenNewTicketsModule)},
-  {path: '', loadChildren: () => import('./sites/sites.module').then(m => m.SitesModule)} // for default redirection
 ];
 
 @NgModule({
